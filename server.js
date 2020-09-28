fa39dcfe887e8c8167515b818be57456f98bb41d
carey <schiafang@gmail.com>
2020-09-28 19:23:06 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server")
+const path = require("path")
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, "db.json"))
+const middlewares = jsonServer.defaults()
+
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+
+server.use(router)
+
+server.listen(3000, () => console.log("JSON Server is running"))
